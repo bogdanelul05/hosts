24069ccab03065227b9daea5eab03842a78e2388
Barbulescu Bogdan <bogdan.barbulescu05@gmail.com>
2024-11-17 14:02:28 +0000

    Primul commit pentru verificarea validitatii adreselor de IP din /etc/hosts

--- script.sh
+++ script.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
 validare_ip() {
-        [[$1 =~ ^[0.9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]] && return 0 || return 1
+      	while read -r line; do
+		ip=$(echo "$line" | awk '{print$1}')
+		host=$(echo "$line" | awk '{print $2}')
+
+		if [[ "$ip" =~ ^[0.9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]
+			echo "IP valid"
+		else
+			echo "IP invalid"
+		fi
+	done < /etc/hosts
 }
 
-while IFS= read -r line do
-        [[ $line =! ^# ]] && [[ -n $line ]]; then
-                ip=$(echo $line | awk '{print $1}')
-                hostname=$(echo $line | awk '{print $2}')
-                if validare_ip "$ip"; then
-                        echo "IP valid: $ip pentru $hostname"
-                else
-                        echo "IP invalid: $ip pentru $hostname"
-                fi
-        fi
-done < etc/hosts
+validare_ip
 
